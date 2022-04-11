3fce17b37a6becc44a6f70f2a2928650ee719140
jonathanjf <jonathanfernandesjf98@gmail.com>
2022-04-11 12:02:59 -0300

    criação e estilizacao do header

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+  return (
+    <nav className="nav-margin">
+      <div class="nav-wrapper teal">
+        <a href="#" class="brand-logo center ">MY WALLET</a>
+      </div>
+  </nav>
+  )
+}
+
+export default Header;
